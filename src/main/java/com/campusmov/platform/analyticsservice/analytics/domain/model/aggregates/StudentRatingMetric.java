2bbfafa7367661bdd73db1151c92ae785402b4dd
Eric <erich56000@gmail.com>
2025-06-14 23:25:50 -0500

    feat(student-metrics): implement update method and builder for student rating metrics entity

--- src/main/java/com/campusmov/platform/analyticsservice/analytics/domain/model/aggregates/StudentRatingMetric.java
+++ src/main/java/com/campusmov/platform/analyticsservice/analytics/domain/model/aggregates/StudentRatingMetric.java
@@ -1,5 +1,6 @@
 package com.campusmov.platform.analyticsservice.analytics.domain.model.aggregates;
 
+import com.campusmov.platform.analyticsservice.analytics.domain.model.commands.CreateStudentRatingMetricsCommand;
 import com.campusmov.platform.analyticsservice.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotBlank;
@@ -15,7 +16,7 @@ public class StudentRatingMetric extends AuditableAbstractAggregateRoot<StudentR
     private String userId;
 
     @NotBlank
-    private Integer totalRatings;
+    private Double totalRatings;
 
     @NotBlank
     private Integer totalReviewsCount;
@@ -24,6 +25,22 @@ public class StudentRatingMetric extends AuditableAbstractAggregateRoot<StudentR
     private Double averageRating;
 
     public StudentRatingMetric() {
+    }
+
+    public StudentRatingMetric(CreateStudentRatingMetricsCommand command) {
+        this.userId = command.userId();
+        this.totalRatings = 5.0;
+        this.totalReviewsCount = 0;
+        this.averageRating = 5.0;
+    }
+
+    public void updateMetrics(Double reputationScore){
+        if (reputationScore == null || reputationScore < 0) {
+            throw new IllegalArgumentException("Reputation score must be a non-negative number");
+        }
+        this.totalRatings += reputationScore;
+        this.totalReviewsCount++;
+        this.averageRating = this.totalRatings / this.totalReviewsCount;
     }
 
 }
